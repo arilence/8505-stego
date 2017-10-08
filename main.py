0f418990674ac4c56d1ec7db9bd707878f8f4176
thomas <thomas__yu@hotmail.com>
2017-10-07 18:05:48 -0700

    Linked GUI with smith's code TODO: Setup gui to receive signal

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ from ui.mainwindow import Ui_MainWindow
 class View(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(View, self).__init__(parent)
+        self.stego = StegoImage()
         self.setupUi(self)
         self.exitButton.clicked.connect(self.exitButtonPressed)
         self.carrierButton.clicked.connect(self.carrierButtonPressed)
         self.secretButton.clicked.connect(self.secretButtonPressed)
-
+        self.encodeFileButton.clicked.connect(self.encodeFileButtonPressed)
+        self.encodeButton.clicked.connect(self.encodeButtonPressed)
+        self.decodeButton.clicked.connect(self.decodeButtonPressed)
     def exitButtonPressed(self):
         sys.exit(0)
     def carrierButtonPressed(self):
@@ -27,15 +30,52 @@ class View(QMainWindow, Ui_MainWindow):
         if file:
             self.carrierEdit.setText(file)
             self.showThumbnail(file, self.carrierImage)
-
-
+        else:
+            sys.exit(1)
     def secretButtonPressed(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         file, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
         if file:
-            self.secretEdit.setText(file,)
-            self.showThumbnail(file, self.secretImage)
+            self.secretEdit.setText(file)
+        else:
+            sys.exit(1)
+    def encodeFileButtonPressed(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if file:
+            self.encodedEdit.setText(file)
+            self.showThumbnail(file, self.encodedImage)
+        else:
+            sys.exit(1)
+
+    def encodeButtonPressed(self):
+        carrierFilePath = self.carrierEdit.text()
+        secretFilePath = self.secretEdit.text()
+        secretFileName = os.path.basename(secretFilePath)
+        outDirectory = os.getcwd() + '/out/' + secretFileName
+        if carrierFilePath and secretFilePath:
+            # Check if hidden file can fit into carrier image
+            carrierSize = os.stat(carrierFilePath).st_size
+            secretSize = os.stat(secretFilePath).st_size
+
+            if ((secretSize * 8) + 264 >= carrierSize - 54):
+                sys.exit(1)
+            newCarrierImage = self.stego.hideSecret(carrierFilePath, secretFilePath, outDirectory)
+            self.encodedEdit.setText(secretFileName)
+        else:
+            sys.exit(1)
+
+    def decodeButtonPressed(self):
+        encodedFilePath = self.encodedEdit.text()
+        if encodedFilePath:
+             self.stego.showSecret(encodedFilePath)
+        else:
+            sys.exit(1)
+
+
+
 
     def showThumbnail(self, file, label):
         thumb = Image.open(file)
